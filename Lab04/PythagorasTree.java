c2f0e1715033c633fad290f87be519dc37d3b4f9
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-04-09 05:18:58 -0500

    mod

---
+++ Lab04/PythagorasTree.java
@@ -0,0 +1,12 @@
+package Lab04;
+import javax.swing.JPanel;
+
+public class PythagorasTree extends JPanel{
+    private int profundidad;
+
+    public PythagorasTree(int profundidad){
+        this.profundidad=profundidad;
+        setPreferredSize(new Dimension(800, 800));
+    }
+    
+}
