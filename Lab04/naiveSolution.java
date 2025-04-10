59453188b3e77050853c3ef6e07a0f029fe29559
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-04-09 22:28:25 -0500

    implementamos el main

--- Lab04/naiveSolution.java
+++ Lab04/naiveSolution.java
@@ -11,5 +11,11 @@ public class naiveSolution {
         }
         return tmpMax;  // Devuelve el valor máximo encontrado
     }
+
+    public static void main(String[] args) {
+        int[] values = new int[]{3, 7, 1, 3, 9};  
+        int rodLength = values.length;  
+        System.out.println("El valor máximo: " + getValue(values, rodLength));
+    }
 }
 
