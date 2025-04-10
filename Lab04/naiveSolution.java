72b50a9ba4227eb6b395be8c1c8fab03561d82a3
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-04-09 22:27:44 -0500

    terminando el for

--- Lab04/naiveSolution.java
+++ Lab04/naiveSolution.java
@@ -3,6 +3,13 @@ package Lab04;
 public class naiveSolution {
     static int getValue(int[] values, int length) {
         if (length <= 0)
-        return 0;
+            return 0;
+        int tmpMax = -1;
+
+        for (int i = 0; i < length; i++) {
+            tmpMax = Math.max(tmpMax, values[i] + getValue(values, length - i - 1));
+        }
+        return tmpMax;  // Devuelve el valor máximo encontrado
     }
 }
+
