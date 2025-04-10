69184961e35248bbc6aba5dcc6cff0c8a2a5f20f
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-04-10 12:14:32 -0500

    metodo quickselect

--- Lab04/QuickSelect.java
+++ Lab04/QuickSelect.java
@@ -3,4 +3,22 @@ public class QuickSelect {
     public static int hallarMenor(int[] arr, int k){
         return quickSelect(arr, 0, arr.length - 1, k -1);
     }
+
+     private static int quickSelect(int[] arr, int left, int rigth, int k){
+        if (left == rigth) {
+            return arr[left];
+        }
+
+        Random random = new Random();
+        int pivote = left + random.nextInt(rigth - left + 1);
+        pivote = dividir(arr, left, rigth, pivote);
+
+        if (k == pivote) {
+            return arr[k];
+        } else if (k < pivote) {
+            return quickSelect(arr, left, pivote -1, k);
+        } else {
+            return quickSelect(arr, pivote + 1, rigth, k);
+        }
+    }
 }
